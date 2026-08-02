14054442da4412fde0041ce8a6f929cda3126ce0
Rifaldi Achmad Faisal <114426626+RifaldiAchmad@users.noreply.github.com>
2026-08-02 20:07:37 +0700

    Update 03_triggers.sql

--- sql_migrations/03_triggers.sql
+++ sql_migrations/03_triggers.sql
@@ -1,1 +1,16 @@
+-- 1. Buat Function
+CREATE OR REPLACE FUNCTION auto_delete_waiting_list()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM perpanjangan_sertifikat
+    WHERE peserta_id = NEW.peserta_id 
+      AND scheme = NEW.scheme;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
+-- 2. Pasang Trigger ke tabel sertifikat
+CREATE TRIGGER hapus_daftar_tunggu_setelah_terbit
+AFTER INSERT ON sertifikat
+FOR EACH ROW
+EXECUTE FUNCTION auto_delete_waiting_list();
