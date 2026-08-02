1d295c7b56d89727d3bd6d879499fc00d3692593
Rifaldi Achmad Faisal <114426626+RifaldiAchmad@users.noreply.github.com>
2026-08-02 20:54:04 +0700

    Update 04_functions.sql

--- sql_migrations/04_functions.sql
+++ sql_migrations/04_functions.sql
@@ -1,1 +1,11 @@
-
+-- Fungsi untuk menghapus asesi dari tabel antrean RCC (Daftar Tunggu) 
+-- ketika sertifikat barunya sudah terbit
+CREATE OR REPLACE FUNCTION auto_delete_waiting_list()
+RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM perpanjangan_sertifikat
+    WHERE peserta_id = NEW.peserta_id 
+      AND scheme = NEW.scheme;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
